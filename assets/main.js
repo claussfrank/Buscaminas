9f0f405febea9d67f18c78b08090a26c5fb95112
Clauss Franco <Clauss Franco>
2017-04-04 18:10:41 -0500

    Funcionan las bombitas

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,35 @@
 //Cambiamos por  medio de  la  clase el  color de la celda
-var longitud= document.getElementsByClassName("nada").length;
-var comment=[];
 
-for (var i=0 ; i<longitud; i++){
-  comment[i] = document.getElementsByClassName("nada")[i];
-}
 
-for (var i=0; i< comment.length; i++){
-  comment[i].addEventListener('click',cambiarColor,false);
-}
-
+var longitudColor = document.getElementsByClassName("nada").length;
+var color=[];
+  for (var i=0 ; i<longitudColor; i++){
+    color[i] = document.getElementsByClassName("nada")[i];
+  }
+  for (var i=0; i< color.length; i++){
+    color[i].addEventListener('click',cambiarColor,false);
+  }
 function cambiarColor(){
-  this.style.backgroundColor = "blue";
+    this.style.backgroundColor = "blue";
 }
+
+
+var longitudBomba = document.getElementsByClassName("bombas").length;
+var bombas=[];
+
+for (var i=0 ; i<longitudBomba; i++){
+  bombas[i] = document.getElementsByClassName("bombas")[i];
+}
+for (var i=0; i< bombas.length; i++){
+  bombas[i].addEventListener('click',ponerBombas);
+}
+function ponerBombas(){
+  var imagen = document.createElement("img");
+  imagen.src = "assets/bomba.png";
+  imagen.style = "width: 10px;height :10px ";
+  this.appendChild(imagen);
+}
+
+
+var longitudNumero = document.getElementsByClassName("numero").length;
+var numero=[];
